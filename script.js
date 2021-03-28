c1807406b906d21dedd5a36fc4f808b7828e09ee
ajamalirad <aplasticlanguage@gmail.com>
2021-03-28 14:09:17 -0400

    API call

--- script.js
+++ script.js
@@ -3,27 +3,34 @@ const wordApp = {}
 // Declare variables
 wordApp.apiUrl = 'https://api.datamuse.com/words';
 wordApp.form = document.querySelector('form')
+const ul = document.querySelector('ul');
 
 wordApp.init = () => {
     wordApp.formSubmit();
 }
 
-wordApp.formSubmit = () => {
-    //  listen for submit event on the form
-    wordApp.form.addEventListener('submit', (event) => {
-        //  prevent page refresh
-        event.preventDefault();
+wordApp.displayWords = (jsonResponse) => {
+    // get first 10 data items from API
+    const words = jsonResponse.slice(0,10);
+    // then create list items and append to ul 
+    words.forEach( (word) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = word.word;
+        ul.appendChild(listItem);
+    });
 
-        // Store values for each input in variables
-        const meaning = document.getElementById('meaning').value;
-        const startsWith = document.getElementById('startsWith').value;
-        const endsWith = document.getElementById('endsWith').value;
-        console.log(meaning, startsWith, endsWith);
-        
-        //  Build url using variables
-        console.log(wordApp.buildUrl(meaning, startsWith, endsWith));
+    // toggle visibility class 
+    const resultsDiv = document.querySelector('.resultsClass');
+    resultsDiv.classList.remove('invisible');
+}
 
-        // wordApp.getWords();
+wordApp.getWords = (url) => {
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonResponse) => {
+        wordApp.displayWords(jsonResponse);
     })
 }
 
@@ -31,22 +38,37 @@ wordApp.buildUrl = (meaning, startsWith, endsWith) => {
     const url = new URL(wordApp.apiUrl);
         url.search = new URLSearchParams({
             ml: meaning,
-            sp: `${startsWith}*${endsWith}`
+            sp: `${startsWith}*${endsWith}`,
         })
-        return url;
+        wordApp.getWords(url);
 }
 
-// wordApp.getWords = () => 
+wordApp.formSubmit = () => {
+    //  listen for submit event on the form
+    wordApp.form.addEventListener('submit', (event) => {
+        //  prevent page refresh
+        event.preventDefault();
+        ul.innerHTML = '';
+        // Store values for each input in variables
+        const meaning = document.getElementById('meaning').value;
+        const startsWith = document.getElementById('startsWith').value;
+        const endsWith = document.getElementById('endsWith').value;
+        //  Build url using variables
+        wordApp.buildUrl(meaning, startsWith, endsWith);
+        const input = document.querySelectorAll('input');
+
+    })
+}
+
+
+
+
+
+
+
 
 
 wordApp.init();
 
 
 
-//         fetch(url)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((jsonResponse) => {
-//             console.log(jsonResponse)
-//         })
