7dc3564039990fd767802dd25e56f5b2b35d5411
AaronHoffman <aahoffm@gmail.com>
2021-03-28 15:11:44 -0400

    added error handling

--- script.js
+++ script.js
@@ -4,24 +4,55 @@ const wordApp = {}
 wordApp.apiUrl = 'https://api.datamuse.com/words';
 wordApp.form = document.querySelector('form')
 const ul = document.querySelector('ul');
+const moreButton = document.querySelector('#moreWords');
+
+//  Helper function to make and display list items
+wordApp.displayItem = (wordsOnPage) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = wordsOnPage;
+    ul.appendChild(listItem);
+}
 
 wordApp.init = () => {
     wordApp.formSubmit();
 }
 
-wordApp.displayWords = (jsonResponse) => {
-    // get first 10 data items from API
-    const words = jsonResponse.slice(0,10);
-    // then create list items and append to ul 
-    words.forEach( (word) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = word.word;
-        ul.appendChild(listItem);
-    });
+wordApp.moreWords = (jsonResponse) => {
+    moreButton.addEventListener('click', () => {
+         //  Get first 10 words
+        const words = jsonResponse.slice (11, 21);
+        // then create list items and append to ul 
+        words.forEach( (word) => {
+        wordApp.displayItem(word.word);
+        })
+    })
+}
 
-    // toggle visibility class 
+
+wordApp.displayWords = (jsonResponse) => {
+    console.log(jsonResponse)
     const resultsDiv = document.querySelector('.resultsClass');
+    //  Check if all inputs are empty
+    if (meaning.value === '' && startsWith.value === '' && endsWith.value === '') {
+        wordApp.displayItem('Fill in at least one field to get results')
+        document.querySelector('li').classList.add('error');
+    } else if (jsonResponse.length === 0) {
+        wordApp.displayItem('Search too specific no results')
+        document.querySelector('li').classList.add('error');
+    } else {
+        //  Get first 10 words
+        const words = jsonResponse.slice(0,10);
+        // then create list items and append to ul 
+        words.forEach( (word) => {
+            wordApp.displayItem(word.word)
+        });
+        // Make button visible
+        moreButton.classList.remove('invisible');
+    }
+    // toggle visibility class 
     resultsDiv.classList.remove('invisible');
+    //  listen for click on more words button and show new words
+    wordApp.moreWords(jsonResponse);
 }
 
 wordApp.getWords = (url) => {
@@ -56,17 +87,8 @@ wordApp.formSubmit = () => {
         //  Build url using variables
         wordApp.buildUrl(meaning, startsWith, endsWith);
         const input = document.querySelectorAll('input');
-
     })
 }
-
-
-
-
-
-
-
-
 
 wordApp.init();
 
