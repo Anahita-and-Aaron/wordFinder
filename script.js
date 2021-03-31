99b9de9c1da588b4e72d0c19563d05eb88489cb3
ajamalirad <aplasticlanguage@gmail.com>
2021-03-31 17:28:26 -0400

    added dictionarygit add -A

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ wordApp.form = document.querySelector('form');
 wordApp.ul = document.querySelector('ul');
 wordApp.moreButton = document.querySelector('#moreWords');
 
+// dictionary variables 
+wordApp.dictUrl = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
+wordApp.dictKey = 'c3f5ec42-dfcc-4f14-a2a6-f7f2df69e673';
+
 //  Helper function to make and display list items
 wordApp.displayItem = (wordsOnPage) => {
     const listItem = document.createElement('li');
@@ -31,10 +35,7 @@ wordApp.moreWords = (jsonResponse) => {
 }
 
 
-
-
 wordApp.displayWords = (jsonResponse) => {
-    console.log(jsonResponse)
     const resultsDiv = document.querySelector('.resultsClass');
     //  Check if all inputs are empty
     if (meaning.value === '' && startsWith.value === '' && endsWith.value === '') {
@@ -95,6 +96,49 @@ wordApp.formSubmit = () => {
         const input = document.querySelectorAll('input');
     })
 }
+
+
+wordApp.getDefinition = () => {
+    wordApp.ul.addEventListener('click', (event) => {
+        const wordInList = event.target.textContent;
+
+        const url = new URL(`${wordApp.dictUrl}${wordInList}`);
+        url.search = new URLSearchParams({
+            key: wordApp.dictKey,
+        })
+
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonResponse) => {
+            wordApp.displayDefinition(jsonResponse[0].shortdef[0], event.target); 
+        })
+        })
+}
+
+wordApp.displayDefinition = (definition, listItem) => {
+    const pElement = document.createElement('p');
+    pElement.classList.add('definition');
+    pElement.textContent = definition;
+    listItem.appendChild(pElement);
+}
+
+
+wordApp.getDefinition();
+// listen for click on list item
+// make api call
+// return response
+// append return onto list item 
+
+
+
+
+
+
+
+
+
 
 wordApp.init();
 
